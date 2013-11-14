1260e5edde5db8956d8fbee64332fc17160663bc
Friederike Wild <dev@friederikewild.me>
2013-11-14 21:36:14 +0000

    * Improved the package name to be more convenient

--- lib/src/de/devmob/android/logger/Logger.java
+++ lib/src/de/devmob/android/logger/Logger.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.devmob.androlib.logger;
+package de.devmob.android.logger;
 
 import android.util.Log;
 
