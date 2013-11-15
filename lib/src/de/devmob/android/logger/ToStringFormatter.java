f33c249107f4c4d38350a26fa9093d5d98b02dac
Friederike Wild <dev@friederikewild.me>
2013-11-15 08:46:55 +0000

    * Take in account that overloaded methods are chosen during compile-time * Support for arrays of all different possible primitive data types

--- lib/src/de/devmob/android/logger/ToStringFormatter.java
+++ lib/src/de/devmob/android/logger/ToStringFormatter.java
@@ -25,8 +25,8 @@ import android.database.Cursor;
 
 /**
  * Formatter to create readable strings from common used objects that
- * lack their own useful toString method. Useful to log collections like lists or maps,
- * native arrays or Android Cursors.
+ * lack their own useful toString method. Useful to log native arrays, Android Cursors or
+ * objects that may be null. Works as well for collections like List or Map.
  * 
  * Call ToStringFormatter.getString() with any kind of object and let it handle the type check.
  * Get a readable string containing the available content instead of the default output that often only
@@ -44,17 +44,17 @@ public class ToStringFormatter
         .toString();
     }
 
-    public static String getString(Collection<?> set)
+    public static String getString(Collection<?> collection)
     {
         StringBuilder sb = new StringBuilder();
 
         sb.append("Collection: (");
         int entryCount = 0;
-        for (Object entry : set)
+        for (Object entry : collection)
         {
             sb.append(getString(entry));
             entryCount++;
-            addEntrySeparatorIfNeeded(entryCount, set.size(), sb);
+            addEntrySeparatorIfNeeded(entryCount, collection.size(), sb);
         }
         sb.append(")");
         return sb.toString();
@@ -82,11 +82,11 @@ public class ToStringFormatter
 
         if (c.getCount() == 0)
         {
-            sb.append("Empty cursor");
+            sb.append("Empty cursor {}");
         }
         else
         {
-            sb.append("Cursor: ");
+            sb.append("Cursor: {");
             // store current position
             int currentPos = c.getPosition();
             c.moveToFirst();
@@ -117,6 +117,8 @@ public class ToStringFormatter
 
             // reset position:
             c.moveToPosition(currentPos);
+
+            sb.append("}");
         }
         return sb.toString();
     }
@@ -128,7 +130,11 @@ public class ToStringFormatter
 
     /**
      * Overloaded method for all otherwise unmapped methods.
-     * Handles null value as well!
+     * In case a specific typed object is stored in a "Object" variable
+     * that overloaded method check is not working due to the determination
+     * of the correct type at compile-time.
+     * 
+     * Therefore this method checks for all other types being instanceof as well.
      * 
      * @param var
      * @return
@@ -158,6 +164,27 @@ public class ToStringFormatter
             return sb.toString();
         }
 
+        else if (var instanceof Collection)
+        {
+            return getString((Collection<?>)var);
+        }
+        
+        else if (var instanceof Map)
+        {
+            return getString((Map<?, ?>)var);
+        }
+
+        else if (var instanceof Cursor)
+        {
+            return getString((Cursor)var);
+        }
+
+        else if (var instanceof Throwable)
+        {
+            return getString((Throwable)var);
+        }
+        
+
         // Else: for all other classes call through to native toString method 
         return var.toString();
     }
@@ -166,11 +193,45 @@ public class ToStringFormatter
     {
         sb.append("[");
 
-        // Special check for boolean array
-        if (var.getClass().toString().startsWith("class [Z"))
+        if (var.getClass().toString().startsWith("class [Z")) // Boolean array prefix
         {
             boolean[] convertedArray = (boolean[]) var;
             dumpBoolArrayToStringBuilder(convertedArray, sb);
+        }
+        else if (var.getClass().toString().startsWith("class [B")) // Byte array prefix
+        {
+            byte[] convertedArray = (byte[]) var;
+            dumpByteArrayToStringBuilder(convertedArray, sb);
+        }
+        else if (var.getClass().toString().startsWith("class [S")) // Short array prefix
+        {
+            short[] convertedArray = (short[]) var;
+            dumpShortArrayToStringBuilder(convertedArray, sb);
+        }
+        else if (var.getClass().toString().startsWith("class [I")) // Int array prefix
+        {
+            int[] convertedArray = (int[]) var;
+            dumpIntArrayToStringBuilder(convertedArray, sb);
+        }
+        else if (var.getClass().toString().startsWith("class [J")) // Long array prefix
+        {
+            long[] convertedArray = (long[]) var;
+            dumpLongArrayToStringBuilder(convertedArray, sb);
+        }
+        else if (var.getClass().toString().startsWith("class [F")) // Float array prefix
+        {
+            float[] convertedArray = (float[]) var;
+            dumpFloatArrayToStringBuilder(convertedArray, sb);
+        }
+        else if (var.getClass().toString().startsWith("class [D")) // Double array prefix
+        {
+            double[] convertedArray = (double[]) var;
+            dumpDoubleArrayToStringBuilder(convertedArray, sb);
+        }
+        else if (var.getClass().toString().startsWith("class [C")) // Char array prefix
+        {
+            char[] convertedArray = (char[]) var;
+            dumpCharArrayToStringBuilder(convertedArray, sb);
         }
         else
         {
@@ -186,7 +247,70 @@ public class ToStringFormatter
     {
         for (int i = 0; i < convertedArray.length; i++)
         {
-            sb.append(convertedArray[i] ? "true" : "false");
+            sb.append(String.valueOf(convertedArray[i]));
+            addEntrySeparatorIfNeeded(i, convertedArray.length - 1, sb);
+        }
+    }
+
+    private static void dumpByteArrayToStringBuilder(byte[] convertedArray, StringBuilder sb)
+    {
+        for (int i = 0; i < convertedArray.length; i++)
+        {
+            sb.append(String.valueOf(convertedArray[i]));
+            addEntrySeparatorIfNeeded(i, convertedArray.length - 1, sb);
+        }
+    }
+
+    private static void dumpShortArrayToStringBuilder(short[] convertedArray, StringBuilder sb)
+    {
+        for (int i = 0; i < convertedArray.length; i++)
+        {
+            sb.append(String.valueOf(convertedArray[i]));
+            addEntrySeparatorIfNeeded(i, convertedArray.length - 1, sb);
+        }
+    }
+
+    private static void dumpIntArrayToStringBuilder(int[] convertedArray, StringBuilder sb)
+    {
+        for (int i = 0; i < convertedArray.length; i++)
+        {
+            sb.append(String.valueOf(convertedArray[i]));
+            addEntrySeparatorIfNeeded(i, convertedArray.length - 1, sb);
+        }
+    }
+
+    private static void dumpLongArrayToStringBuilder(long[] convertedArray, StringBuilder sb)
+    {
+        for (int i = 0; i < convertedArray.length; i++)
+        {
+            sb.append(String.valueOf(convertedArray[i]));
+            addEntrySeparatorIfNeeded(i, convertedArray.length - 1, sb);
+        }
+    }
+
+    private static void dumpFloatArrayToStringBuilder(float[] convertedArray, StringBuilder sb)
+    {
+        for (int i = 0; i < convertedArray.length; i++)
+        {
+            sb.append(String.valueOf(convertedArray[i]));
+            addEntrySeparatorIfNeeded(i, convertedArray.length - 1, sb);
+        }
+    }
+
+    private static void dumpDoubleArrayToStringBuilder(double[] convertedArray, StringBuilder sb)
+    {
+        for (int i = 0; i < convertedArray.length; i++)
+        {
+            sb.append(String.valueOf(convertedArray[i]));
+            addEntrySeparatorIfNeeded(i, convertedArray.length - 1, sb);
+        }
+    }
+
+    private static void dumpCharArrayToStringBuilder(char[] convertedArray, StringBuilder sb)
+    {
+        for (int i = 0; i < convertedArray.length; i++)
+        {
+            sb.append(String.valueOf(convertedArray[i]));
             addEntrySeparatorIfNeeded(i, convertedArray.length - 1, sb);
         }
     }
